df9945392f6725bcbc5ba256645aa247577a663f
Evgeny <evgeny.hyvarinen@tietoevry.com>
2023-12-15 17:54:20 +0200

    Admin cosmetic fixes

--- sys_core/online_queue/apps.py
+++ sys_core/online_queue/apps.py
@@ -1,9 +1,11 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class OnlineQueueConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "online_queue"
+    verbose_name = _("Online queue")
 
     def ready(self) -> None:
         import online_queue.signals
